d4c0b2c835251acd6840555421a3308c541495f5
hailong11 <hailong11@staff.weibo.com>
2020-01-02 13:05:53 +0800

    up

--- features/user/up.sh
+++ features/user/up.sh
@@ -2,7 +2,15 @@
 
 TAG=latest
 PROJECT=hailongz/kk-user
-rm -rf main
+
 echo "[GO BUILD] [$PROJECT:$TAG] >>"
 docker run --rm -v `pwd`:/main:rw -v $GOPATH:/go:rw hailongz/kk-gobuild:latest go build
 echo "[OK]"
+echo "[DOCKER BUILD] [$PROJECT:$TAG] >>"
+docker build -t $PROJECT:$TAG .
+echo "[OK]"
+echo "[DOCKER PUSH] [$PROJECT:$TAG] >>"
+docker push $PROJECT:$TAG
+echo "[OK]"
+rm -rf main
+
